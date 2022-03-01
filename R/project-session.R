e9fc4fae05e43609000d16211c3ade768d4b6810
Sidsel Louise Domazet <sidsel.louise.domazet@gmail.com>
2022-03-01 16:09:30 +0100

    R learning projects

---
+++ R/project-session.R
@@ -0,0 +1,68 @@
+
+# Call the packages
+source(here::here("R/package-loading.R"))
+
+
+# R project management  ---------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos
+
+# Character vector
+c("a", "b", "c")
+
+# Logic vector
+c(TRUE, FALSE, FALSE)
+
+# Factor vector
+factor(c("low", "high", "medium", "high"))
+
+head(airquality)
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+colnames(airquality)
+
+# 4.10 Exercise: Make code more readable
+
+# Object names
+Day_1
+
+false <- FALSE
+number_value <- 9
+
+# Spacing
+x[, 1]
+x[, 1]
+mean(x, na.rm = TRUE)
+mean(x, na.rm = TRUE )
+
+height <- feet * 12 + inches
+
+df$z
+
+x <- 1:10
+
+
+# Use Reformat Code (ctrl+shift+A)
+# Object names
+DayOne
+T <- FALSE
+c <- 9
+
+# Spacing
+x[, 1]
+x[, 1]
+mean (x, na.rm = TRUE)
+mean(x, na.rm = TRUE)
+height <- feet * 12 + inches
+df$z
+x <- 1:10
+
+library(tidyverse)
+
+usethis::use_r("package-loading")
