bbfdddb1d31f8b4ae781cdacee43a711731b4773
Sidsel Louise Domazet <sidsel.louise.domazet@gmail.com>
2022-03-02 12:00:27 +0100

    Exercise done

---
+++ R/exercises-wrangling.R
@@ -0,0 +1,20 @@
+# Data script (Wrangling)
+
+# Replace the ___ with NHANES
+
+source(here::here("R/package-loading.R"))
+
+# Check column names
+colnames(NHANES)
+
+# Look at contents
+str(NHANES)
+glimpse(NHANES)
+
+# See summary
+summary(NHANES)
+
+# Look over the dataset documentation
+?NHANES
+
+?select()
