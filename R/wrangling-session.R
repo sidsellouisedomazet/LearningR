4a1a06832220d0081514edb0395ad7e3934572cc
Sidsel Louise Domazet <sidsel.louise.domazet@gmail.com>
2022-03-02 15:05:04 +0100

    group_by

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -127,4 +127,10 @@ nhanes_small %>%
     summarise(max_bmi=max(bmi, na.rm = TRUE),
               min_bmi=min(bmi, na.rm = TRUE))
 
+nhanes_small %>%
+    filter(!is.na(diabetes)) %>%
+    group_by(diabetes, phys_active) %>%
+    summarise(mean_age=mean(age, na.rm = TRUE),
+              mean_bmi=mean(bmi, na.rm = TRUE)) %>%
+    ungroup()
 
